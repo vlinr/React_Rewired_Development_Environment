471e78fae50c4e547b687de3a9541f89925c8a7d
李忠贤 <282204626@qq.com>
2020-08-06 18:32:15 +0800

    update fetch

--- src/request/Request.ts
+++ src/request/Request.ts
@@ -1,9 +1,10 @@
 import qs from 'qs';
 
+declare const method: ['GET', 'POST', 'DELETE', 'PUT', 'TRACE', 'OPTIONS', 'HEAD', 'CONNECT', 'get', 'post', 'delete', 'put', 'trace', 'options', 'head', 'connect'];
 export interface RequestParams {
     url?: string  //请求的前缀，也就是域名，如果使用通用域名则可以不传
     api: string,  //请求的api
-    method?: string, //请求的方式
+    method?: typeof method[number], //请求的方式
     cors?: boolean, //是否跨域共享资源
     headers?: any,  //设置请求头，一个对象
     data?: any, //请求数据，一个对象
@@ -31,7 +32,7 @@ class Request {
     //请求参数
     private requestParams: RequestParams = defaultParams;
     constructor(params: RequestParams) {
-        this.requestParams = {...this.requestParams,...params};
+        this.requestParams = { ...this.requestParams, ...params };
     }
 
     //请求数据
@@ -43,7 +44,12 @@ class Request {
                         headers: new Headers(this.requestParams.headers),
                         method: this.requestParams.method,
                         mode: this.requestParams.cors ? 'cors' : 'no-cors'
-                    }).then(res => res?.json?.())
+                    }).then(res => {
+                        if (!res.ok) {
+                            reject(res.statusText);
+                        }
+                        return res?.json?.();
+                    })
                         .catch(error => reject(error))
                         .then(response => resolve(response))
                     :
@@ -52,7 +58,12 @@ class Request {
                         method: this.requestParams.method,
                         body: this.requestParams.dataType == 'json' ? qs.stringify(this.requestParams.data) : this.requestParams.data, // data can be `string` or {object}!
                         mode: this.requestParams.cors ? 'cors' : 'no-cors'
-                    }).then(res => res?.json?.())
+                    }).then(res => {
+                        if (!res.ok) {
+                            reject(res.statusText);
+                        }
+                        return res?.json?.();
+                    })
                         .catch(error => reject(error))
                         .then(response => resolve(response));
             } catch (error) {
